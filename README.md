2087330cfec1f8ca461e2016997dda09c5a85d3d
PARLIN <152256286+LIN21-ARTZY@users.noreply.github.com>
2024-11-24 09:55:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Parlin-2210010348-UTS
+ UTS Parlin (2210010348)
---
+++ src/parlin/pkg2210010348/uts/Parlin2210010348UTS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parlin.pkg2210010348.uts;
+
+/**
+ *
+ * @author Acer
+ */
+public class Parlin2210010348UTS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new AplikasiCatatanHarian().setVisible(true);// TODO code application logic here
+    }
+    
+}
